0a99355fb1152934f0c9794c23b5c40088fb790a
MaCarina <75248048+MaCarina@users.noreply.github.com>
2021-09-18 19:11:05 +0200

    Ajout final du dossier

---
+++ Source/Principal.c
@@ -0,0 +1,22 @@
+
+#include "stm32f10x.h"
+int main ( void )
+{
+	char  pin ;
+RCC->APB2ENR |= (0x01 << 2) | (0x01 << 3) | (0x01 << 4) ;
+
+	pin =5 ;
+	GPIOA->CRL &= ~(0xF << (pin*4);
+	GPIOA->CRL |= (0x1 << (pin*4) | (0x2 <<(oin;
+	
+	
+	
+	GPIOC->CRH &= ~(0xF << 20);
+	GPIOC->CRH |= (0x1 << 20);
+	
+	while (1)
+{
+} ;
+
+
+}
