a1043b976f6130d419f1f9fabba1d5e1f4922cee
MaCarina <75248048+MaCarina@users.noreply.github.com>
2021-09-19 21:38:48 +0200

    Update Principal.c
    Initialisation GPIO

--- Source/Principal.c
+++ Source/Principal.c
@@ -6,17 +6,17 @@ int main ( void )
 RCC->APB2ENR |= (0x01 << 2) | (0x01 << 3) | (0x01 << 4) ;
 
 	pin =5 ;
-	GPIOA->CRL &= ~(0xF << (pin*4);
-	GPIOA->CRL |= (0x1 << (pin*4) | (0x2 <<(oin;
+	GPIOA->CRL &= ~(0xF << (pin*4));
+	GPIOA->CRL |= (0x1 << (pin*4)) | (0x2 <<(pin*4+2));
 	
 	
 	
-	GPIOC->CRH &= ~(0xF << 20);
-	GPIOC->CRH |= (0x1 << 20);
+	GPIOC->CRH &= ~(0xF << (pin*4));
+	GPIOC->CRH |= (0x1 << (pin*4));
 	
 	while (1)
 {
-} ;
+}
 
 
 }
