5fbfbec62775d2d1574a829ffb51ad96fb445077
MaCarina <75248048+MaCarina@users.noreply.github.com>
2021-09-19 21:52:58 +0200

    Essai principal.c
    Essai principal.c

--- Source/Principal.c
+++ Source/Principal.c
@@ -5,7 +5,7 @@ int main ( void )
 	char  pin ;
 RCC->APB2ENR |= (0x01 << 2) | (0x01 << 3) | (0x01 << 4) ;
 
-	pin =5 ;
+	pin = 5 ;
 	GPIOA->CRL &= ~(0xF << (pin*4));
 	GPIOA->CRL |= (0x1 << (pin*4)) | (0x2 <<(pin*4+2));
 	
